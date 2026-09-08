fa9e37fd5e19c971b2ee30adbc3c4e28d1a0c7e5
Cecilie Kaendo <lathoyaHijamutiti@gmail.com>
2026-09-08 16:21:53 -0600

    Edit Type error on BookTest.java

--- dmit2015-javase-demo/src/test/java/dmit2015/model/BookTest.java
+++ dmit2015-javase-demo/src/test/java/dmit2015/model/BookTest.java
@@ -15,9 +15,15 @@ class BookTest {
         Book currentBook = new Book("Java Fundamentals",59.99);
         // Act
         double actualPrice = currentBook.getPrice();
-        //Assert
+        // Assert
         assertThat(actualPrice)
-                .isCloseTo(59.99, )
+                .isCloseTo(59.99, within(0.01));
     }
+    @Test
+    void constructor_whenPriceIsNegative_shouldThrowException() {
+        assertThatThrownBy(() ->
+                new Book("Invalid Price",-1.49)
+        ).isInstanceOf(IllegalArgumentException.class);
 
+    }
 }
